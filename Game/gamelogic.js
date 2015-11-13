d0b99d0fd9c986a6f5c7880872762c00d9d85585
Devanist <devanist@outlook.com>
2015-11-13 19:01:12 +0100

    Rewrote two functions getting each player score as parametrized one

--- Game/gamelogic.js
+++ Game/gamelogic.js
@@ -71,19 +71,12 @@ define(['Core/Logic/Logic'], function(LogicCore){
     GameLogic.DRAW = 3;
 	
 	/**
-		Funkcja zwracająca wynik czerwonego gracza
+		Funkcja zwracająca wynik danego gracza czerwonego gracza
+		@param {string} player Nazwa gracza
 		@returns {int}
 	*/
-	_p.getRedScore = function(){
-		return this.getPlayer("Red").currency("Score").getAmount();
-	};
-	
-	/**
-		Funkcja zwracająca wynik zielonego gracza
-		@returns {int}
-	*/
-	_p.getGreenScore = function(){
-		return this.getPlayer("Green").currency("Score").getAmount();
+	_p.getPlayerScore = function(player){
+		return this.getPlayer(player).currency("Score").getAmount();
 	};
 	
 	/**
